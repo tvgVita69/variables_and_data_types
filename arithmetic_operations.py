92063ee2a7ccc70b865a38e0e098d2757542bb81
Vitaly <98489171+tvgVita69@users.noreply.github.com>
2023-12-31 16:39:43 +0300

    Update arithmetic_operations.py

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -17,3 +17,19 @@ print(pow(z, 2), z ** 2)  # квадрат числа
 
 m = 3.26
 print(round(m), round(m, 1))  # округление числа до целого и до одного знака после запятой
+
+
+import math  # импротируем модуль math
+
+x = 3.265
+# целое число, ближайшее целое снизу, ближайшее целое сверху
+print(math.trunc(x), math.floor(x), math.ceil(x))
+
+print(math.pi)  # константа пи
+print(math.e)   # число Эйлера
+
+y = math.sin(math.pi / 4)  # math.sin – синус
+print(round(y, 2))
+
+y = 1 / math.sqrt(2)  # math.sqrt – квадратный корень
+print(round(y, 2))
